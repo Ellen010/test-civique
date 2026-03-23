713881eeadd948e22bbcb014e30df45875874b09
Ellen010 <elenawelty@gmail.com>
2026-03-23 17:40:39 +0100

    Hero set up

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero.tsx'
+import './index.css';
+
+
+function App() {
+    return (
+                        <>
+                            <Hero />
+                        </>
+
+    );
+};
+
+
+export default App
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <section id="hero">
-            <div className="flex justify-center text-lg"> Pass Your French (2026 Guide) </div>
+        <section id="hero" className="flex items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-white">
+                        Pass Your French (2026 Guide)
+                    </h1>
         </section>
     )
 }
