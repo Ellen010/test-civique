b403fdfebcb29040284ac94f5c5f5c28812b33dc
Elena WELTY <157579304+Ellen010@users.noreply.github.com>
2026-03-28 19:00:56 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,8 @@ import Puppy from "../../public/puppy-one.png";
 
 const Hero = () => {
     return (
-        <section id="hero" className="w-full h-screen flex items-center justify-center">
-            <div className="text-center">
+        <section id="hero" className="general">
+            <div className="items-center">
                 <h1 className="text-8xl font-bold chango">
                     Pass Your French (2026 Guide)
                 </h1>
